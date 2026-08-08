26cc5f08cb57aaa5ea64ae164af3b1fda0bba373
bluestar021592 <bluestar021592@gmail.com>
2026-08-09 00:16:50 +0700

    V29: load data consistency patch before server start

--- bootstrap.js
+++ bootstrap.js
@@ -74,6 +74,7 @@ try {
   await import('./src/v27CarryBusinessPatch.js');
   await import('./src/v28ResumeGuardPatch.js');
   await import('./src/v28RuntimePatch.js');
+  await import('./src/v29DataConsistencyPatch.js');
   await import('./server.js');
 } catch (error) {
   console.error('[CE-QC][STARTUP_FATAL]', error?.stack || error);
