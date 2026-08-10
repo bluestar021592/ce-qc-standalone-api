d7012c633b93566e56c0d72a11b51c0687757dc4
bluestar021592 <bluestar021592@gmail.com>
2026-08-10 23:47:19 +0700

    perf: install cold-start indexes before server launch

--- bootstrap.js
+++ bootstrap.js
@@ -92,6 +92,7 @@ try {
   await importPhase('v42WhppPatch', './src/v42WhppPatch.js');
   await importPhase('v44WhppUiPatch', './src/v44WhppUiPatch.js');
   await importPhase('v43BootstrapPerfPatch', './src/v43BootstrapPerfPatch.js');
+  await importPhase('v46ColdStartIndexPatch', './src/v46ColdStartIndexPatch.js');
   await importPhase('server', './server.js');
 } catch (error) {
   console.error('[CE-QC][STARTUP_FATAL]', error?.stack || error);
