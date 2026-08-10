74e1288d48950835444f353cdcd0b7299d7cf79a
bluestar021592 <bluestar021592@gmail.com>
2026-08-10 21:40:30 +0700

    perf: load V43 lightweight bootstrap patch before server

--- bootstrap.js
+++ bootstrap.js
@@ -90,6 +90,7 @@ try {
   await importPhase('v39UnifiedSnapshotRecoveryPatch', './src/v39UnifiedSnapshotRecoveryPatch.js');
   await importPhase('v41AuthPausePatch', './src/v41AuthPausePatch.js');
   await importPhase('v42WhppPatch', './src/v42WhppPatch.js');
+  await importPhase('v43BootstrapPerfPatch', './src/v43BootstrapPerfPatch.js');
   await importPhase('server', './server.js');
 } catch (error) {
   console.error('[CE-QC][STARTUP_FATAL]', error?.stack || error);
