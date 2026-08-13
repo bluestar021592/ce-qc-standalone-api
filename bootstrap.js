0787e80309f4943352e8f0fd7af2c5e51568e520
bluestar021592 <bluestar021592@gmail.com>
2026-08-13 12:01:25 +0700

    feat: register Shopee WHPP SQL metric route before server listen

--- bootstrap.js
+++ bootstrap.js
@@ -104,6 +104,7 @@ try {
   await importPhase('v70ConfirmQueryResiliencePatch', './src/v70ConfirmQueryResiliencePatch.js');
   await importPhase('v71WhppSummaryPatch', './src/v71WhppSummaryPatch.js');
   await importPhase('v84AsyncExportPatch', './src/v84AsyncExportPatch.js');
+  await importPhase('v85ShopeeWhppMetricPatch', './src/v85ShopeeWhppMetricPatch.js');
   await importPhase('v73CeafSourceMarkerPatch', './src/v73CeafSourceMarkerPatch.js');
   await importPhase('v74CeafDuplicateReimportPatch', './src/v74CeafDuplicateReimportPatch.js');
   const v76Repair = await importPhase('v76CurrentCeafSplitRepair', './src/v76CurrentCeafSplitRepair.js');
