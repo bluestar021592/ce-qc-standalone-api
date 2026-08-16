1098714cc69f139e927e471840ebeaa45ed7f77e
bluestar021592 <bluestar021592@gmail.com>
2026-08-16 23:54:16 +0700

    fix: load V163 Shopee daily isolation before run routes

--- bootstrap.js
+++ bootstrap.js
@@ -114,6 +114,7 @@ try {
   await importPhase('v29EndpointAliasPatch', './src/v29EndpointAliasPatch.js');
   await importPhase('v30CarryRulesPatch', './src/v30CarryRulesPatch.js');
   await importPhase('v162ShopeeLiveProgressPatch', './src/v162ShopeeLiveProgressPatch.js');
+  await importPhase('v163ShopeeDailyIsolationPatch', './src/v163ShopeeDailyIsolationPatch.js');
   await importPhase('v33RunProgressPatch', './src/v33RunProgressPatch.js');
   await importPhase('v39UnifiedSnapshotRecoveryPatch', './src/v39UnifiedSnapshotRecoveryPatch.js');
   await importPhase('v41AuthPausePatch', './src/v41AuthPausePatch.js');
