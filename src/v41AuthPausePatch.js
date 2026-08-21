cea88bb97a83697ef3150fd58a4c85efead86a5e
bluestar021592 <bluestar021592@gmail.com>
2026-08-21 16:48:38 +0700

    QC11: load bounded CE API login on direct 5177 shell

--- src/v41AuthPausePatch.js
+++ src/v41AuthPausePatch.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import './qc11CeLoginBoundPatch.js';
 import { clearTokenSync } from './authStore.js';
 import { getCurrentReportDate, updateRunLock } from './store.js';
 import { SHOPEE, getBusinessCurrentReportDate, updateBusinessRunLock } from './businessStore.js';
