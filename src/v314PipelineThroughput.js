9f14cee71701ef7c937846bc1653dbbb89321120
bluestar021592 <bluestar021592@gmail.com>
2026-08-27 20:57:06 +0700

    V339 route CCSL through hard-bounded prefetch core

--- src/v314PipelineThroughput.js
+++ src/v314PipelineThroughput.js
@@ -1,18 +1,21 @@
 import { runQcPipeline as runOriginalQcPipeline } from './pipeline.js';
 import {
-  createCcslThroughputClient,
   createShopeeThroughputClient,
   V314_SHOPEE_THROUGHPUT_CORE_ID,
-  V339_CCSL_THROUGHPUT_CORE_ID,
   V314_CONFIRM_CONCURRENCY,
   V314_EVENT_CONCURRENCY,
-  V314_EXCEPTION_CONCURRENCY,
-  V339_CCSL_CONFIRM_CONCURRENCY
+  V314_EXCEPTION_CONCURRENCY
 } from './v314ShopeeThroughputCore.js';
+import {
+  createCcslThroughputClient,
+  V339_CCSL_THROUGHPUT_CORE_ID,
+  V339_CCSL_CONFIRM_CONCURRENCY,
+  V339_CCSL_CONFIRM_HARD_BUDGET_MS
+} from './v339CcslThroughputCore.js';
 export * from './pipeline.js';
 
 export const V314_PIPELINE_THROUGHPUT_ID = '2026-08-26-v314-shopee-prefetch-pipeline-v1';
-export const V339_CCSL_PIPELINE_THROUGHPUT_ID = '2026-08-27-v339-ccsl-350x2-prefetch-track50x4-v1';
+export const V339_CCSL_PIPELINE_THROUGHPUT_ID = '2026-08-27-v339-ccsl-350x2-prefetch-track50x4-v2';
 
 export async function runQcPipeline(options = {}) {
   const state = options.state || {};
@@ -42,6 +45,6 @@ console.info('[CE-QC][V339_CCSL_THROUGHPUT]', JSON.stringify({
   scanConcurrency: V339_CCSL_CONFIRM_CONCURRENCY,
   trackBatchSize: 50,
   trackConcurrency: Math.max(1, Number(process.env.TRACK_CONCURRENCY || 4)),
-  confirmHardBudgetMs: Math.max(1, Number(process.env.CE_CONFIRM_HARD_BUDGET_MS || 18000)),
-  policy: 'CCSL_CONFIRM_350_X2_PREFETCH_ORDERED_NATIVE_COMMIT_TRACK_50_X4_FAIL_FORWARD'
+  confirmHardBudgetMs: V339_CCSL_CONFIRM_HARD_BUDGET_MS,
+  policy: 'CCSL_CONFIRM_350_X2_HARD_BOUNDED_PREFETCH_ORDERED_NATIVE_COMMIT_TRACK_50_X4_FAIL_FORWARD'
 }));
