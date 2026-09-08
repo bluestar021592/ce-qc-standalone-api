d61f26812e547c5312f69fddbdb724574806dd2b
bluestar021592 <bluestar021592@gmail.com>
2026-09-08 16:00:27 +0700

    V473 cache-bust final isolated export UI

--- public/v194-export-token-ui.js
+++ public/v194-export-token-ui.js
@@ -1,9 +1,9 @@
 (function installV473ExportUiLoader(global){
-  const VERSION='2026-09-08-v473-export-ui-loader-v1';
+  const VERSION='2026-09-08-v473-export-ui-loader-v2';
   if(global.__CE_QC_V473_EXPORT_UI_LOADER__===VERSION)return;
   global.__CE_QC_V473_EXPORT_UI_LOADER__=VERSION;
   const script=document.createElement('script');
-  script.src='/v473-all-export-sidecar-ui.js?v=20260908-v473-1';
+  script.src='/v473-all-export-sidecar-ui.js?v=20260908-v473-2';
   script.async=false;
   script.dataset.ceQcExportOwner='v473';
   (document.head||document.documentElement).appendChild(script);
