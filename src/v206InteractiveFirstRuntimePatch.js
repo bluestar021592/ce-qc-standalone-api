3e2cf7c9cc5af900d75f9e4f8c76dfde0d515ff1
bluestar021592 <bluestar021592@gmail.com>
2026-09-10 13:41:35 +0700

    V501 wire pre-purge manual tracking guard before V246

--- src/v206InteractiveFirstRuntimePatch.js
+++ src/v206InteractiveFirstRuntimePatch.js
@@ -1,6 +1,7 @@
 import { spawn } from 'node:child_process';
 import { fileURLToPath } from 'node:url';
 import './v499FreshStartTrackingPolicy.js';
+import './v501FreshStartManualTrackingGate.js';
 import './v266EvergreenEvidenceArchive.js';
 import './v234DashboardLiveTruthPatch.js';
 import './v236DashboardCurrentRoutePatch.js';
