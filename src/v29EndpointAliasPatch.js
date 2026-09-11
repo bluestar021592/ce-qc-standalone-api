ec89f75df1e9060cc6f37c1d59c7d0c87afedeee
bluestar021592 <bluestar021592@gmail.com>
2026-09-11 08:08:27 +0700

    V505 wire global purge owner guard into prepare and execute routes

--- src/v29EndpointAliasPatch.js
+++ src/v29EndpointAliasPatch.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { v505PurgePrepareHandler, v505PurgeExecuteHandler, v505PurgeWriteBlockMiddleware, V505_PURGE_COORDINATOR_ID } from './v505PurgeCoordinator.js';
+import { v505PurgeGlobalOwnerGuard, V505_PURGE_GLOBAL_GUARD_ID } from './v505PurgeGlobalGuard.js';
 
 let installed=false;
 let purgeWriteBlockInstalled=false;
@@ -26,10 +27,10 @@ const previousPost=express.application.post;
 express.application.post=function v505PurgeRoutePost(pathValue,...handlers){
   const route=String(pathValue||'');
   if(route==='/api/admin/data-purge/prepare'&&handlers.length){
-    return previousPost.call(this,pathValue,...handlers.slice(0,-1),v505PurgePrepareHandler);
+    return previousPost.call(this,pathValue,...handlers.slice(0,-1),v505PurgeGlobalOwnerGuard,v505PurgePrepareHandler);
   }
   if(route==='/api/admin/data-purge/execute'&&handlers.length){
-    return previousPost.call(this,pathValue,...handlers.slice(0,-1),v505PurgeExecuteHandler);
+    return previousPost.call(this,pathValue,...handlers.slice(0,-1),v505PurgeGlobalOwnerGuard,v505PurgeExecuteHandler);
   }
   return previousPost.call(this,pathValue,...handlers);
 };
@@ -48,4 +49,4 @@ express.application.listen=function v29EndpointAliasListen(...args){
   return previousListen.apply(this,args);
 };
 
-console.info('[CE-QC][V505_PURGE_COORDINATOR_ROUTE_OWNER]',V505_PURGE_COORDINATOR_ID);
+console.info('[CE-QC][V505_PURGE_COORDINATOR_ROUTE_OWNER]',V505_PURGE_COORDINATOR_ID,V505_PURGE_GLOBAL_GUARD_ID);
