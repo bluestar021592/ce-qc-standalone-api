b757217c52121e7a512c56635c88d8ac30f5b1cc
bluestar021592 <bluestar021592@gmail.com>
2026-09-18 14:48:36 +0700

    Stability: cache-bust lazy V473 export sidecar UI

--- public/v194-export-token-ui.js
+++ public/v194-export-token-ui.js
@@ -3,7 +3,7 @@
   if(global.__CE_QC_V473_EXPORT_UI_LOADER__===VERSION)return;
   global.__CE_QC_V473_EXPORT_UI_LOADER__=VERSION;
   const script=document.createElement('script');
-  script.src='/v473-all-export-sidecar-ui.js?v=20260908-v473-2';
+  script.src='/v473-all-export-sidecar-ui.js?v=20260918-lazy-sidecar-1';
   script.async=false;
   script.dataset.ceQcExportOwner='v473';
   (document.head||document.documentElement).appendChild(script);
