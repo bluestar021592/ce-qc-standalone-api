9b9fbc88da34a095da07d61f1c5bd92741e14e02
bluestar021592 <bluestar021592@gmail.com>
2026-09-22 16:55:36 +0700

    V581 cache-bust local login shell from launcher

--- Start_CE_QC.ps1
+++ Start_CE_QC.ps1
@@ -172,7 +172,7 @@ $env:HOST = '0.0.0.0'
 $env:PORT = '5177'
 $LocalUrl = 'http://127.0.0.1:5177'
 $RecoveryLaunchMode = ([string]$env:CE_QC_OPEN_PURGE_CONSOLE).Trim() -eq '1'
-$LaunchUrl = if ($RecoveryLaunchMode) { "$LocalUrl/purge-console.html?v=20260920-recovery-first" } else { "$LocalUrl/local-login.html?v=20260921-v575-1" }
+$LaunchUrl = if ($RecoveryLaunchMode) { "$LocalUrl/purge-console.html?v=20260920-recovery-first" } else { "$LocalUrl/local-login.html?v=20260922-v581-1" }
 
 function Archive-BackendLogs([string]$Reason) {
     $stamp = Get-Date -Format 'yyyyMMdd-HHmmss'
